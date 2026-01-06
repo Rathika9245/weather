c756bc9b4e56236961c94c0fc4ae3542c24b061d
Raththicka Gansham <raththickagansham9245@gmail.com>
2026-01-06 15:06:14 +0530

    update

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -21,7 +21,6 @@ function Weather() {
     setLoading(true);
     setError("");
 
-
     try {
       const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
